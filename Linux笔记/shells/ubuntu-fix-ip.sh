2b39ef01b3ff128857d15267b49ba728e70647ae
林蔚湘 <itweixiang@126.com>
2022-07-17 13:17:36 +0800

    操作系统初始化脚本修改

---
+++ Linux笔记/shells/ubuntu-fix-ip.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+HOSTNAME="copy"
+DNS1="192.168.0.3"
+GATEWAY="192.168.0.2"
+IPADDR="192.168.0.73"
+NETMASK="255.255.254.0"
+
+echo "network:
+  ethernets:
+    enp0s3:  #配置的网卡的名称
+      dhcp4: no  #dhcp4关闭
+      addresses: [${IPADDR}/24]  #设置本机IP及掩码，/24即表示24位掩码255.255.255.0
+      gateway4: ${GATEWAY}  #网关IP，设置为和物理机相同
+      nameservers:
+        addresses: [${DNS1}]  #设置DNS，与物理机相同
+  version: 2" > /etc/netplan/00-installer-config.yaml
+
+
+hostnamectl set-hostname ${HOSTNAME}
