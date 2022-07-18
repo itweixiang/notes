ec35ab19209d1969738469aaa8972aefc88a9172
林蔚湘 <itweixiang@126.com>
2022-07-18 20:55:19 +0800

    ubuntu脚本修改

--- Linux笔记/shells/ubuntu-fix-ip.sh
+++ Linux笔记/shells/ubuntu-fix-ip.sh
@@ -5,10 +5,11 @@ DNS1="192.168.0.3"
 GATEWAY="192.168.0.2"
 IPADDR="192.168.0.73"
 NETMASK="255.255.254.0"
+NETCARD="?"
 
 echo "network:
   ethernets:
-    enp0s3:  #配置的网卡的名称
+    ${NETCARD}:  #配置的网卡的名称
       dhcp4: no  #dhcp4关闭
       addresses: [${IPADDR}/24]  #设置本机IP及掩码，/24即表示24位掩码255.255.255.0
       gateway4: ${GATEWAY}  #网关IP，设置为和物理机相同
